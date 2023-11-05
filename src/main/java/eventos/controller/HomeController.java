1bf3796b04398386096a148e57006db99bb9ffd4
SergioRL <sergiorl1989@gmail.com>
2023-11-05 13:29:57 +0100

    All methods commented

--- src/main/java/eventos/controller/HomeController.java
+++ src/main/java/eventos/controller/HomeController.java
@@ -13,21 +13,38 @@ import eventos.modelo.javabeans.Evento;
 
 @Controller
 public class HomeController {
+	
 	@Autowired
 	private EventoDao edao;
 
+	
+	/**
+	 * MOSTRAR PAGINA PRINCIPAL (HOME)
+	 * 
+	 * Muestra la página de inicio (Home) con la lista de eventos activos y
+	 * cancelados.
+	 * 
+	 * Utiliza el método findAll para obtener todos los eventos. Luego, separa los
+	 * eventos en dos listas, una para eventos activos y otra para eventos
+	 * cancelados, basándose en el estado de cada evento. Agrega las listas al
+	 * modelo y muestra la vista "home".
+	 * 
+	 * @param model (el modelo utilizado para pasar datos a la vista)
+	 * @return home (vista de la página de inicio con la lista de eventos activos y
+	 *         cancelados)
+	 */
 	@GetMapping("/")
 	public String mostrarHome(Model model) {
-		List<Evento> eActivo = new ArrayList<>();
-		List<Evento> eCancelado = new ArrayList<>();
+		List<Evento> evActivo = new ArrayList<>();
+		List<Evento> evCancelado = new ArrayList<>();
 		for (Evento evento : edao.findAll()) {
 			if (evento.getEstado().equalsIgnoreCase("activo")) {
-				eActivo.add(evento);
+				evActivo.add(evento);
 			} else
-				eCancelado.add(evento);
+				evCancelado.add(evento);
 		}
-		model.addAttribute("eventosActivos", eActivo);
-		model.addAttribute("evCancelados", eCancelado);
+		model.addAttribute("eventosActivos", evActivo);
+		model.addAttribute("evCancelados", evCancelado);
 		return "home";
 	}
 
